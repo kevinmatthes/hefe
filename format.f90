5298ed6b3f4b45f6eaf90f962d3e35cfde6c2b3f
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-09-22 00:47:32 +0200

    Update format.f90

--- format.f90
+++ format.f90
@@ -18,8 +18,9 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 SUBMODULE (LIBHEFE) FORMAT
-    USE, NON_INTRINSIC :: LIBHEFE, ONLY: RESET => ESCAPE_RESET
     USE, NON_INTRINSIC :: LIBHEFE, ONLY: INIT_TEXT
+    USE, NON_INTRINSIC :: LIBHEFE, ONLY: RESET     => ESCAPE_RESET
+    USE, NON_INTRINSIC :: LIBHEFE, ONLY: RESIZE
 IMPLICIT NONE
 CONTAINS
     PURE SUBROUTINE BOLD_TEXT_RESET (TEXT)
@@ -27,17 +28,51 @@ CONTAINS
     IMPLICIT NONE
         CHARACTER (:), ALLOCATABLE                 :: BUFFER
         CHARACTER (:), INTENT (INOUT), ALLOCATABLE :: TEXT
-        INTEGER                                    :: STRLEN
         INTRINSIC                                  :: ACHAR
         INTRINSIC                                  :: LEN
 
         CALL INIT_TEXT (TEXT, BUFFER)
-        STRLEN = LEN (TEXT) + LEN (BOLD) + LEN (RESET) + 2
-        DEALLOCATE (TEXT)
-        ALLOCATE (CHARACTER (STRLEN) :: TEXT)
+        CALL RESIZE (TEXT, LEN (BOLD) + LEN (BUFFER) + LEN (RESET) + 2)
         TEXT = ACHAR (27) // BOLD // BUFFER // ACHAR (27) // RESET
         DEALLOCATE (BUFFER)
     END SUBROUTINE BOLD_TEXT_RESET
+
+    PURE SUBROUTINE CURSOR_TEXT (TEXT, X, Y)
+        USE, NON_INTRINSIC :: LIBHEFE, ONLY: STRINGIFY
+    IMPLICIT NONE
+        CHARACTER (:), ALLOCATABLE                 :: BUFFER
+        CHARACTER (:), ALLOCATABLE                 :: X_BUFFER
+        CHARACTER (:), ALLOCATABLE                 :: Y_BUFFER
+        CHARACTER (:), INTENT (INOUT), ALLOCATABLE :: TEXT
+        INTEGER                                    :: X_VALUE
+        INTEGER                                    :: Y_VALUE
+        INTEGER, INTENT (IN)                       :: X
+        INTEGER, INTENT (IN)                       :: Y
+        INTRINSIC                                  :: ACHAR
+        INTRINSIC                                  :: LEN
+
+        CALL INIT_TEXT (TEXT, BUFFER)
+        CALL INIT_TEXT ('', X_BUFFER)
+        CALL INIT_TEXT ('', Y_BUFFER)
+        X_VALUE = X
+        Y_VALUE = Y
+
+        IF (X_VALUE <= 0) THEN
+            X_VALUE = 1
+        END IF
+
+        IF (Y_VALUE <= 0) THEN
+            Y_VALUE = 1
+        END IF
+
+        CALL STRINGIFY (X_VALUE, X_BUFFER)
+        CALL STRINGIFY (Y_VALUE, Y_BUFFER)
+        CALL RESIZE (TEXT, LEN (Y_BUFFER) + LEN (X_BUFFER) + LEN (BUFFER) + 4)
+        TEXT = ACHAR (27) // '[' // Y_BUFFER // ';' // X_BUFFER // 'H' // BUFFER
+        DEALLOCATE (BUFFER)
+        DEALLOCATE (X_BUFFER)
+        DEALLOCATE (Y_BUFFER)
+    END SUBROUTINE CURSOR_TEXT
 END SUBMODULE FORMAT
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
